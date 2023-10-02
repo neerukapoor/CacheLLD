e42df2b18716acd794981ba816c17bf0447c4cb3
Neeru Kapoor <467neeru@gmail.com>
2023-10-02 19:48:03 +0530

    Basic Structure

---
+++ cache.java
@@ -0,0 +1,23 @@
+import java.util.*;  
+
+class Cache {
+
+    public void get(string key) {
+
+    }
+
+    public void put(string key, string value)
+
+
+}
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);  
+        System.out.println("Input an integer ");
+        int a= sc.nextInt(); 
+        System.out.println("a is " + a);
+
+    }
+}
